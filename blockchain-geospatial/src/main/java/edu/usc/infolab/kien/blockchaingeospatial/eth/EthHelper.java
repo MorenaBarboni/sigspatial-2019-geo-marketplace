53cbda7bc6290c3aa4ce17118b25118ccccadf21
Morena <morena.barboni@studenti.unicam.it>
2022-06-16 09:35:51 +0200

    Added buyer credentials

--- blockchain-geospatial/src/main/java/edu/usc/infolab/kien/blockchaingeospatial/eth/EthHelper.java
+++ blockchain-geospatial/src/main/java/edu/usc/infolab/kien/blockchaingeospatial/eth/EthHelper.java
@@ -98,6 +98,8 @@ public class EthHelper {
             Credentials credentialsCurator = Credentials.create("0x6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1");
             Credentials credentialsBuyer = Credentials.create("0x6370fd033278c143179d81c5526140625662b8daa446c22ee2d73db3707e620c");
             Credentials credentialsBuyer2 = Credentials.create("0x646f1ce2fdad0e6deeeb5c7e8e5543bdde65e86029e2fd9fc169899c440a7913");
+            Credentials credentialsBuyer3 = Credentials.create("0xe485d098507f54e7733a205420dfddbe58db035fa577fc294ebd14db90767a52");
+            Credentials credentialsBuyer4 = Credentials.create("0xa453611d9419d0e56f499079478fd72c37b251a94bfde4d19872c44cf65386e3");
             Credentials credentialsOwner = Credentials.create("0xadd53f9a7e588d003326d1cbf9e4a43c061aadd9bc938c843a79e7b4fd2ad743");
 
             //Load funding credentials
@@ -124,10 +126,13 @@ public class EthHelper {
             ownerAccounts.add(credentialsOwner);
             buyerAccounts.add(credentialsBuyer);
             buyerAccounts.add(credentialsBuyer2);
+            buyerAccounts.add(credentialsBuyer3);
+            buyerAccounts.add(credentialsBuyer4);
             allCredentials.add(credentialsOwner);
             allCredentials.add(credentialsBuyer);
             allCredentials.add(credentialsBuyer2);
-
+            allCredentials.add(credentialsBuyer3);
+            allCredentials.add(credentialsBuyer4);
 
             logger.info("There are " + ownerAccounts.size() + " owner accounts");
             logger.info("There are " + buyerAccounts.size() + " buyer accounts");
